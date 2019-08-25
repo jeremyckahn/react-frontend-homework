9846245af669568161ca63febce457bf633520ee
Jeremy Kahn <jeremyckahn@gmail.com>
2019-08-25 13:21:49 -0500

    Refactor hotel DOM into Hotel component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,35 @@ import './App.style.scss';
 
 import hotelResultService from '../../services/hotel-result/hotel-result.service';
 
+const Hotel = ({
+  hotel: { id, hotelStaticContent, lowestAveragePrice, rewards },
+}) => (
+  <div className="hotel-card" key={id}>
+    <div
+      className="image"
+      style={{
+        backgroundImage: `url(${hotelStaticContent.mainImage.url})`,
+      }}
+    ></div>
+    <div className="hotel-details">
+      <div className="hotel-name">{hotelStaticContent.name}</div>
+      <div className="location">{hotelStaticContent.neighborhoodName}</div>
+    </div>
+    <div className="price-details">
+      <span className="price">
+        <span
+          dangerouslySetInnerHTML={{
+            __html: lowestAveragePrice.symbol,
+          }}
+        ></span>
+        {lowestAveragePrice.amount}
+      </span>
+      <span className="rewards">{rewards.miles} miles</span>
+      <button className="button">Select</button>
+    </div>
+  </div>
+);
+
 const App = () => {
   const [hotels, setHotels] = useState([]);
 
@@ -31,34 +60,7 @@ const App = () => {
 
         <div className="hotel-list">
           {hotels.map(hotel => (
-            <div className="hotel-card" key={hotel.id}>
-              <div
-                className="image"
-                style={{
-                  backgroundImage: `url(${hotel.hotelStaticContent.mainImage.url})`,
-                }}
-              ></div>
-              <div className="hotel-details">
-                <div className="hotel-name">
-                  {hotel.hotelStaticContent.name}
-                </div>
-                <div className="location">
-                  {hotel.hotelStaticContent.neighborhoodName}
-                </div>
-              </div>
-              <div className="price-details">
-                <span className="price">
-                  <span
-                    dangerouslySetInnerHTML={{
-                      __html: hotel.lowestAveragePrice.symbol,
-                    }}
-                  ></span>
-                  {hotel.lowestAveragePrice.amount}
-                </span>
-                <span className="rewards">{hotel.rewards.miles} miles</span>
-                <button className="button">Select</button>
-              </div>
-            </div>
+            <Hotel {...{ hotel, key: hotel.id }} />
           ))}
         </div>
       </div>
